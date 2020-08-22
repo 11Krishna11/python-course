c484804140df662671f725a2b029e2e9cea094c4
Coding_Basics <67582752+CodingBasics698@users.noreply.github.com>
2020-08-22 15:54:16 +0530

    Update and rename main.py to basics.py

---
+++ basics.py
@@ -0,0 +1,10 @@
+print('Hello world')
+
+# here 'Hello world' is a string that contains only textual data.
+
+message = 'Hello World'
+print(message)
+
+# Here message is a variable that has a value, that is 'Hello world'. And notice that when you are going to 
+# print any variable you don't have to use any inverted commas.An also we use underscore to separate variables.
+
--- main.py
+++
@@ -1,4 +0,0 @@
-# student = {'name': 'John', 'age': 25, 'courses': ['Maths', 'CompSci']}
-
-# for key, value in student.items():
-#   print(key,'-', value)
